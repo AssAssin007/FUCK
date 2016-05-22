67c184889a8262d94e1db590a193b354362d9895
your name <your@email.com>
2016-05-22 13:30:07 +0800

    项目初始爱护

---
+++ 1/1/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  1
+//
+//  Created by 石伟 on 16/5/22.
+//  Copyright © 2016年 石伟. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    NSLog(@"项目初始化");
+}
+
+@end
